b512e7f8769282eedeb2cce0481222514e4ca3cf
ardonis <43009291+ardonis@users.noreply.github.com>
2018-09-12 12:57:36 +0200

    Update slicing.py

--- slicing.py
+++ slicing.py
@@ -1,14 +1,19 @@
 def newSlice(strIn, sliceAt):
 
     lastsubstr = ""
+
     output = []
 
     for i in range(len(strIn)):
-        if strIn[i:i+len(sliceAt)]==sliceAt:
+
+        lastsubstr = lastsubstr + strIn[i]
+        if strIn[i:i+len(sliceAt)] == sliceAt:
             output.append(lastsubstr)
             lastsubstr = ""
-        lastsubstr = lastsubstr + strIn[i]
+
     output.append(lastsubstr)
+
     return output
 
-print(newSlice(input("string"),input("slice where?")))
+
+print(newSlice(input("string"), input("slice where?")))
